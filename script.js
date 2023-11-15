ad76fec8f7e6e6bf66dddd1e2342c98dce53328b
evechotiya <130030248+evechotiya@users.noreply.github.com>
2023-11-15 18:14:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var sidemeu = document.getElementById("sidemenu");
+
+    function openmenu(){
+        sidemenu.style.right = "0";
+
+}
+function closemenu(){
+        sidemenu.style.right = "-200px";
+
+}
